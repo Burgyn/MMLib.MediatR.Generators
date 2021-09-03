cb706a5fdb9b735f41cb8c94035a86a48666fab5
Milan Martiniak <mino.martiniak@gmail.com>
2021-09-03 10:14:52 +0200

    Integration tests

---
+++ tests/MMLib.MediatR.Generators.Tests/AssemblyHelper.cs
@@ -0,0 +1,23 @@
+﻿using System.IO;
+using System.Reflection;
+using System.Text;
+
+namespace MMLib.MediatR.Generators.Tests
+{
+    public static class AssemblyHelper
+    {
+        private const string RootNamespaceResources = "MMLib.MediatR.Generators.Tests.Resources";
+
+        /// <summary>
+        /// Gets the string from resource file asynchronous.
+        /// </summary>
+        /// <param name="resourceFile">The resource file name.</param>
+        public static string GetStringFromResourceFileAsync(string resourceFile)
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+            var resourceStream = assembly.GetManifestResourceStream($"{RootNamespaceResources}.{resourceFile}");
+            using var reader = new StreamReader(resourceStream, Encoding.UTF8);
+            return reader.ReadToEnd();
+        }
+    }
+}
---
+++ tests/MMLib.MediatR.Generators.Tests/Controllers/IntegrationTests.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using System.Collections.Immutable;
+using System.Reflection;
+using System.Threading.Tasks;
+using MMLib.MediatR.Generators.Controllers;
+using VerifyXunit;
+using Xunit;
+
+namespace MMLib.MediatR.Generators.Tests.Controllers
+{
+    [UsesVerify]
+    public class IntegrationTests
+    {
+        private static Compilation CreateCompilation(string source)
+            => CSharpCompilation.Create("compilation",
+                new[] { CSharpSyntaxTree.ParseText(source) },
+                new[] { MetadataReference.CreateFromFile(typeof(Binder).GetTypeInfo().Assembly.Location) },
+                new CSharpCompilationOptions(OutputKind.ConsoleApplication));
+
+        [Theory]
+        [InlineData("AssemblyWithoutMediatR")]
+        [InlineData("AssemblyWithOneController")]
+        [InlineData("AssemblyWithMultipleControllers")]
+        public Task GeneratorShouldGenerateCorrectClasses(string sourceCodeFile)
+        {
+            var sourceCode = AssemblyHelper.GetStringFromResourceFileAsync($"{sourceCodeFile}.txt");
+            var result = RunGenerator(sourceCode);
+            
+            return Verifier.Verify(result)
+                .UseParameters(sourceCodeFile);
+        }
+
+        private static ImmutableArray<GeneratedSourceResult> RunGenerator(string sourceCode)
+        {
+            var compilation = CreateCompilation(sourceCode);
+            GeneratorDriver driver = CSharpGeneratorDriver.Create(new ControllersGenerator());
+            driver = driver.RunGeneratorsAndUpdateCompilation(compilation, out var _, out var _);
+
+            var runResult = driver.GetRunResult();
+
+            return runResult.Results[0].GeneratedSources;
+        }
+    }
+}
